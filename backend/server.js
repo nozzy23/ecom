0193eae8d7247b08db65a5d72aaf0c5174884c99
Oscar Nunez <noscar07@gmail.com>
2021-01-04 21:01:08 -0500

    file structure

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log (`Server running on port ${PORT}`));
