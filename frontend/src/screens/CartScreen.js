e1c6d1f89d68e3a78d09657a66745196ed3d0c5e
Oscar Nunez <noscar07@gmail.com>
2021-01-12 23:16:29 -0500

    finished product screen, working on cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,22 @@
 import "./CartScreen.css";
+import CartItem from "../components/CartItem"
 
 const CartScreen = () => {
-    return <div className="cartscreen">Cart Screen!
+    return <div className="cartscreen">
+        <div className="cartscreen__left">
+            <h2>Shopping cart</h2>
+
+            <CartItem />
+        </div>
+        <div className="cartscreen__right">
+            <div className="cartscreen__info">
+            <p>Subtotal (0) items</p>
+            <p>$499.00</p>
+            </div>
+            <div>
+                <button>Proceed to check out</button>
+            </div>
+        </div>
     </div>
 };
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,11 @@
 import "./ProductScreen.css";
 
 const ProductScreen = () => {
-    return <div className="productScreen">
+    return <div className="productscreen">
                 <div className="productscreen__left">
                     <div className="left__image">
                 <img 
-                src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"  alt="product name"
-                />
+                src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"  alt="product name"/>
             </div>
 
             <div className="left__info">
