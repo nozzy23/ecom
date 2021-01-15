9610553dd57aede5d1b99a06582605d18bc0e0c8
Oscar Nunez <noscar07@gmail.com>
2021-01-14 22:04:51 -0500

    setting up redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ frontend/src/redux/reducers/cartReducer.js
@@ -0,0 +1,4 @@
+import * as actionTypes from '../constants/cartConstants';
+
+
+export const cartReducer = (state = { })
