777983f297fc60fb5b34a1af1fe69611c2fe07b4
Oscar Nunez <noscar07@gmail.com>
2021-01-26 23:34:50 -0500

    wowgit add *  im done! i did it

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -14,8 +14,17 @@ const reducer = combineReducers({
 
 const middleware = [thunk];
 
+const cartFromLocalStorage = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []
+
+const INITIAL_STATE = {
+    cart: {
+        cartItems: cartFromLocalStorage
+    }
+}
+
 const store = createStore(
     reducer,
+    INITIAL_STATE,
     composeWithDevTools(applyMiddleware(...middleware))
 )
 
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -41,6 +41,7 @@ const CartScreen = () => {
             ): ( 
                 cartItems.map((item) =>
                 <CartItem 
+                key={item.product}
                 item={item} 
                 qtyChangeHandler={qtyChangeHandler} 
                 removeHandler={removeHandler}/>)
