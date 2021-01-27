2f0809de82801eaf482037de98495467c686d079
Oscar Nunez <noscar07@gmail.com>
2021-01-26 21:52:53 -0500

    finish adding functionality on product screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,15 +1,14 @@
 import "./CartScreen.css";
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 
+//components
 import CartItem from "../components/CartItem";
 
 const CartScreen = () => {
     return <div className="cartscreen">
         <div className="cartscreen__left">
             <h2>Shopping cart</h2>
-
-            <CartItem />
-            <CartItem />
-            <CartItem />
         </div>
         <div className="cartscreen__right">
             <div className="cartscreen__info">
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import "./ProductScreen.css";
 import {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import { useSelector, useDispatch} from 'react-redux';
 
 //Actions
 import { getProductDetails} from '../redux/actions/productActions';
@@ -15,50 +15,61 @@ const ProductScreen = ({match, history}) => {
     const {loading, error, product} = productDetails;
 
     useEffect(() => {
-        if(product && match.parm.id !== product._id) {
-            dispatch(getProductDetails(match.params.id));
+        if (product && match.params.id !== product._id) {
+          dispatch(getProductDetails(match.params.id));
         }
-    }, [dispatch, product, match]);
+      }, [dispatch, match, product]);
+
+      const addToCartHandler = () => {
+          dispatch(addToCart(product._id, qty));
+          history.push("/cart");
+      }
 
 
-    return <div className="productscreen">
-                <div className="productscreen__left">
+    return ( <div className="productscreen">
+        {loading ? <h2>Loading!!!</h2> : error ? <h2>{error}</h2> : (
+            <>
+    <div className="productscreen__left">
                     <div className="left__image">
                 <img 
-                src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"  alt="product name"/>
+                src={product.imageURL} 
+                alt={product.name}/>
             </div>
 
             <div className="left__info">
-                <p className="left__name">product 1</p>
-                <p>price:$69.69</p>
-                <p> lets go go go go go go go go go gog og og og go go jrijhguirjeho g go grjds</p>
+                <p className="left__name">{product.name}</p>
+            <p>price:${product.price}</p>
+                <p> Description: {product.description} </p>
             </div>
         </div>
         <div className="productscreen__right">
             <div className="right__info">
                 <p>
-                    price: <span>69.00$</span>
+            price: <span>${product.price}</span>
                 </p>
                 <p>
-                    Status:<span>In stock</span>
+            Status:<span>{product.countInStock > 0 ? "In Stock" : "Out of stock"}</span>
                 </p>
                 <p>
                     Qty
-                    <select>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
+                    <select value ={qty} onChange={(e) => setQty(e.target.value)}>
+                        {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x+1} value={x+1}>{x+1}</option>
+                        ))};
                     </select>
                     <p>
-                        <button type="button">
+                        <button type="button" onClick={addToCartHandler}>
                         Add to cart
                         </button>
                     </p>
                 </p>
             </div>
         </div>
+                </>
+        )}
+                
     </div>
+        )
 };
 
 export default ProductScreen;
