17b3063c8e748e3b424c05f40067797fe59e8691
Oscar Nunez <noscar07@gmail.com>
2021-01-26 22:50:09 -0500

    working on cartscreen, specifically option

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,29 @@
 import "./CartScreen.css";
 import {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 //components
 import CartItem from "../components/CartItem";
 
 const CartScreen = () => {
-    return <div className="cartscreen">
+        const dispatch = useDispatch();
+
+        const cart = useSelector(state => state.cart);
+        const { cartItems } = cart;
+
+    return (
+    <div className="cartscreen">
         <div className="cartscreen__left">
             <h2>Shopping cart</h2>
+            {cartItems.length === 0 ? (
+                <div>
+                    Your Cart is empty <Link to ="/"> Go Back </Link>
+                </div>
+            ): ( 
+                cartItems.map((item) =>
+                <CartItem item={item}/>)
+            )}
         </div>
         <div className="cartscreen__right">
             <div className="cartscreen__info">
@@ -20,6 +35,7 @@ const CartScreen = () => {
             </div>
         </div>
     </div>
+    );
 };
 
 export default CartScreen;
